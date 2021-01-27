13240a65f7b5600c41db0564e390a5fb41dab2ec
Víctor Alfonso Martí <victor19am@gmail.com>
2021-01-27 14:46:45 +0100

    Agregar archivos de proyecto.

---
+++ T21_UNIT_TEST/Calculadora_Areas.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace T21_UNIT_TEST
+{
+    partial class Calculadora_Areas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boton_cuadrado = new System.Windows.Forms.Button();
+            this.boton_circulo = new System.Windows.Forms.Button();
+            this.boton_triangulo = new System.Windows.Forms.Button();
+            this.boton_rectangulo = new System.Windows.Forms.Button();
+            this.boton_pentagono = new System.Windows.Forms.Button();
+            this.boton_rombo = new System.Windows.Forms.Button();
+            this.boton_romboide = new System.Windows.Forms.Button();
+            this.boton_trapecio = new System.Windows.Forms.Button();
+            this.panel_datos = new System.Windows.Forms.Panel();
+            this.calcular = new System.Windows.Forms.Button();
+            this.numero3 = new System.Windows.Forms.TextBox();
+            this.texto3 = new System.Windows.Forms.Label();
+            this.numero2 = new System.Windows.Forms.TextBox();
+            this.numero1 = new System.Windows.Forms.TextBox();
+            this.texto2 = new System.Windows.Forms.Label();
+            this.texto1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel_datos.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.14286F);
+            this.label1.Location = new System.Drawing.Point(141, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(906, 102);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Áreas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.label2.Location = new System.Drawing.Point(12, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(396, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Selecciona un Área";
+            // 
+            // boton_cuadrado
+            // 
+            this.boton_cuadrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_cuadrado.Location = new System.Drawing.Point(36, 302);
+            this.boton_cuadrado.Name = "boton_cuadrado";
+            this.boton_cuadrado.Size = new System.Drawing.Size(334, 87);
+            this.boton_cuadrado.TabIndex = 2;
+            this.boton_cuadrado.Text = "Cuadrado";
+            this.boton_cuadrado.UseVisualStyleBackColor = true;
+            this.boton_cuadrado.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_circulo
+            // 
+            this.boton_circulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_circulo.Location = new System.Drawing.Point(36, 395);
+            this.boton_circulo.Name = "boton_circulo";
+            this.boton_circulo.Size = new System.Drawing.Size(334, 87);
+            this.boton_circulo.TabIndex = 3;
+            this.boton_circulo.Text = "Círculo";
+            this.boton_circulo.UseVisualStyleBackColor = true;
+            this.boton_circulo.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_triangulo
+            // 
+            this.boton_triangulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_triangulo.Location = new System.Drawing.Point(36, 488);
+            this.boton_triangulo.Name = "boton_triangulo";
+            this.boton_triangulo.Size = new System.Drawing.Size(334, 87);
+            this.boton_triangulo.TabIndex = 4;
+            this.boton_triangulo.Text = "Triángulo";
+            this.boton_triangulo.UseVisualStyleBackColor = true;
+            this.boton_triangulo.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_rectangulo
+            // 
+            this.boton_rectangulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_rectangulo.Location = new System.Drawing.Point(36, 581);
+            this.boton_rectangulo.Name = "boton_rectangulo";
+            this.boton_rectangulo.Size = new System.Drawing.Size(334, 87);
+            this.boton_rectangulo.TabIndex = 5;
+            this.boton_rectangulo.Text = "Rectángulo";
+            this.boton_rectangulo.UseVisualStyleBackColor = true;
+            this.boton_rectangulo.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_pentagono
+            // 
+            this.boton_pentagono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_pentagono.Location = new System.Drawing.Point(36, 674);
+            this.boton_pentagono.Name = "boton_pentagono";
+            this.boton_pentagono.Size = new System.Drawing.Size(334, 87);
+            this.boton_pentagono.TabIndex = 6;
+            this.boton_pentagono.Text = "Pentágono";
+            this.boton_pentagono.UseVisualStyleBackColor = true;
+            this.boton_pentagono.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_rombo
+            // 
+            this.boton_rombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_rombo.Location = new System.Drawing.Point(36, 767);
+            this.boton_rombo.Name = "boton_rombo";
+            this.boton_rombo.Size = new System.Drawing.Size(334, 87);
+            this.boton_rombo.TabIndex = 7;
+            this.boton_rombo.Text = "Rombo";
+            this.boton_rombo.UseVisualStyleBackColor = true;
+            this.boton_rombo.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_romboide
+            // 
+            this.boton_romboide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_romboide.Location = new System.Drawing.Point(36, 860);
+            this.boton_romboide.Name = "boton_romboide";
+            this.boton_romboide.Size = new System.Drawing.Size(334, 87);
+            this.boton_romboide.TabIndex = 8;
+            this.boton_romboide.Text = "Romboide";
+            this.boton_romboide.UseVisualStyleBackColor = true;
+            this.boton_romboide.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // boton_trapecio
+            // 
+            this.boton_trapecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.14286F);
+            this.boton_trapecio.Location = new System.Drawing.Point(36, 953);
+            this.boton_trapecio.Name = "boton_trapecio";
+            this.boton_trapecio.Size = new System.Drawing.Size(334, 87);
+            this.boton_trapecio.TabIndex = 9;
+            this.boton_trapecio.Text = "Trapecio";
+            this.boton_trapecio.UseVisualStyleBackColor = true;
+            this.boton_trapecio.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // panel_datos
+            // 
+            this.panel_datos.Controls.Add(this.calcular);
+            this.panel_datos.Controls.Add(this.numero3);
+            this.panel_datos.Controls.Add(this.texto3);
+            this.panel_datos.Controls.Add(this.numero2);
+            this.panel_datos.Controls.Add(this.numero1);
+            this.panel_datos.Controls.Add(this.texto2);
+            this.panel_datos.Controls.Add(this.texto1);
+            this.panel_datos.Location = new System.Drawing.Point(475, 305);
+            this.panel_datos.Name = "panel_datos";
+            this.panel_datos.Size = new System.Drawing.Size(652, 520);
+            this.panel_datos.TabIndex = 10;
+            this.panel_datos.Visible = false;
+            // 
+            // calcular
+            // 
+            this.calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.14286F);
+            this.calcular.Location = new System.Drawing.Point(62, 412);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(536, 94);
+            this.calcular.TabIndex = 8;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            // 
+            // numero3
+            // 
+            this.numero3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.numero3.Location = new System.Drawing.Point(295, 219);
+            this.numero3.Name = "numero3";
+            this.numero3.Size = new System.Drawing.Size(334, 55);
+            this.numero3.TabIndex = 7;
+            this.numero3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // texto3
+            // 
+            this.texto3.AutoSize = true;
+            this.texto3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.texto3.Location = new System.Drawing.Point(3, 219);
+            this.texto3.Name = "texto3";
+            this.texto3.Size = new System.Drawing.Size(147, 51);
+            this.texto3.TabIndex = 6;
+            this.texto3.Text = "Altura:";
+            // 
+            // numero2
+            // 
+            this.numero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.numero2.Location = new System.Drawing.Point(295, 126);
+            this.numero2.Name = "numero2";
+            this.numero2.Size = new System.Drawing.Size(334, 55);
+            this.numero2.TabIndex = 4;
+            this.numero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numero1
+            // 
+            this.numero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.numero1.Location = new System.Drawing.Point(295, 33);
+            this.numero1.Name = "numero1";
+            this.numero1.Size = new System.Drawing.Size(334, 55);
+            this.numero1.TabIndex = 3;
+            this.numero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // texto2
+            // 
+            this.texto2.AutoSize = true;
+            this.texto2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.texto2.Location = new System.Drawing.Point(3, 126);
+            this.texto2.Name = "texto2";
+            this.texto2.Size = new System.Drawing.Size(147, 51);
+            this.texto2.TabIndex = 2;
+            this.texto2.Text = "Altura:";
+            // 
+            // texto1
+            // 
+            this.texto1.AutoSize = true;
+            this.texto1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.14286F);
+            this.texto1.Location = new System.Drawing.Point(3, 33);
+            this.texto1.Name = "texto1";
+            this.texto1.Size = new System.Drawing.Size(133, 51);
+            this.texto1.TabIndex = 1;
+            this.texto1.Text = "Base:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.resultado);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(527, 880);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(577, 160);
+            this.panel2.TabIndex = 0;
+            this.panel2.Visible = false;
+            // 
+            // resultado
+            // 
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.14286F);
+            this.resultado.Location = new System.Drawing.Point(28, 67);
+            this.resultado.Name = "resultado";
+            this.resultado.ReadOnly = true;
+            this.resultado.Size = new System.Drawing.Size(518, 74);
+            this.resultado.TabIndex = 1;
+            this.resultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.14286F);
+            this.label3.Location = new System.Drawing.Point(161, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 55);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Resultado";
+            // 
+            // Calculadora_Areas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 1073);
+            this.Controls.Add(this.panel_datos);
+            this.Controls.Add(this.boton_trapecio);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.boton_romboide);
+            this.Controls.Add(this.boton_rombo);
+            this.Controls.Add(this.boton_pentagono);
+            this.Controls.Add(this.boton_rectangulo);
+            this.Controls.Add(this.boton_triangulo);
+            this.Controls.Add(this.boton_circulo);
+            this.Controls.Add(this.boton_cuadrado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Calculadora_Areas";
+            this.Text = "Geometria";
+            this.panel_datos.ResumeLayout(false);
+            this.panel_datos.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button boton_cuadrado;
+        private System.Windows.Forms.Button boton_circulo;
+        private System.Windows.Forms.Button boton_triangulo;
+        private System.Windows.Forms.Button boton_rectangulo;
+        private System.Windows.Forms.Button boton_pentagono;
+        private System.Windows.Forms.Button boton_rombo;
+        private System.Windows.Forms.Button boton_romboide;
+        private System.Windows.Forms.Button boton_trapecio;
+        private System.Windows.Forms.Panel panel_datos;
+        private System.Windows.Forms.Label texto2;
+        private System.Windows.Forms.Label texto1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox numero2;
+        private System.Windows.Forms.TextBox numero1;
+        private System.Windows.Forms.TextBox numero3;
+        private System.Windows.Forms.Label texto3;
+        private System.Windows.Forms.Button calcular;
+    }
+}
+
---
+++ T21_UNIT_TEST/Calculadora_Areas.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T21_UNIT_TEST
+{
+    public partial class Calculadora_Areas : Form
+    {
+        public Calculadora_Areas()
+        {
+            InitializeComponent();
+        }
+
+        private void boton_Click(object sender, EventArgs e)
+        {
+            // MÉTODO usado por todos los botones para llamar al respectivo método para calcular
+            // según el método mostramos unos labels u otros para pedir los datos
+            
+            Button boton = (Button)sender;
+
+            panel2.Hide(); // ocultamos el panel de resultado por si ya se había mostrado antes
+            // reiniciamos los textbox de inserción de números
+            numero1.Text = "";
+            numero2.Text = "";
+            numero3.Text = "";
+
+            // recogemos el nombre del boton para printar unos textos u otros
+            // también cambiamos los nombres de los costados por cada polígono
+            switch (boton.Name)
+            {
+                case "boton_cuadrado":
+                    texto1.Text = "Lado:";
+                    texto1.Show();
+                    texto2.Hide();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Hide();
+                    numero3.Hide();
+                    break;
+                case "boton_circulo":
+                    texto1.Text = "Radio:";
+                    texto1.Show();
+                    texto2.Hide();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Hide();
+                    numero3.Hide();
+                    break;
+                case "boton_triangulo":
+                    texto1.Text = "Base:";
+                    texto2.Text = "Altura:";
+                    texto1.Show();
+                    texto2.Show();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Show();
+                    numero3.Hide();
+                    break;
+                case "boton_rectangulo":
+                    texto1.Text = "Base:";
+                    texto2.Text = "Altura:";
+                    texto1.Show();
+                    texto2.Show();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Show();
+                    numero3.Hide();
+                    break;
+                case "boton_pentagono":
+                    texto1.Text = "Apotema:";
+                    texto2.Text = "Lado:";
+                    texto1.Show();
+                    texto2.Show();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Show();
+                    numero3.Hide();
+                    break;
+                case "boton_rombo":
+                    texto1.Text = "Diagonal Mayor:";
+                    texto2.Text = "Diagonal Menor:";
+                    texto2.Show();
+                    texto2.Show();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Show();
+                    numero3.Hide();
+                    // reajustamos el tamaño de los textbox
+                    numero1.Size = new System.Drawing.Size(125,55);
+                    numero1.Location = new Point(210,18); 
+                    numero2.Size = new System.Drawing.Size(125, 55);
+                    numero2.Location = new Point(210, 68);
+                    break;
+                case "boton_romboide":
+                    texto1.Text = "Base:";
+                    texto2.Text = "Altura:";
+                    texto2.Show();
+                    texto2.Show();
+                    texto3.Hide();
+                    numero1.Show();
+                    numero2.Show();
+                    numero3.Hide();
+                    break;
+                case "boton_trapecio":
+                    texto1.Text = "Base Menor:";
+                    texto2.Text = "Base Mayor:";
+                    texto3.Text = "Altura:";
+                    // reajustamos el tamaño de los textbox
+                    numero1.Size = new System.Drawing.Size(155, 55);
+                    numero2.Size = new System.Drawing.Size(155, 55);
+                    numero1.Location = new Point(190, 18);
+                    numero2.Location = new Point(190, 68);
+                    texto2.Show();
+                    texto2.Show();
+                    texto3.Show();
+                    numero1.Show();
+                    numero2.Show();
+                    numero3.Show();
+                    break;
+                default:
+                    break;
+            }
+
+            // añadimos el evento onclick para poder enviar a ejecutar las comprobaciones
+            calcular.Click += (sender2, EventArgs) => { calcular_Click(sender2, EventArgs, boton.Name, numero1.Text, numero2.Text, numero3.Text); };
+                        
+            panel_datos.Show(); // mostramos el panel para que el usuario ponga los datos
+        }
+
+        private void calcular_Click(object sender, EventArgs e, string nom, string numero1, string numero2, string numero3)
+        {
+            // botó de calcular, cridem les diverses funcions per calcular
+
+            // inicializamos variables vacias para enviarlas por parámetros en los métodos
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            // si nos han entrado algún contenido en las cajas de texto, si no contenido = 0
+            // intentamos convertir el string en int, si no, da error
+            if (numero1 != "")
+            {
+                try
+                {
+                    num1 = Convert.ToInt32(numero1);
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Entrada de " + texto1.Text + " no reconeguda.");
+                }
+            }
+            if (numero2 != "")
+            {
+                try
+                {
+                    num2 = Convert.ToInt32(numero2);
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Entrada de " + texto2.Text + " no reconeguda.");
+                }
+            }
+            if (numero3 != "")
+            {
+                try
+                {
+                    num3 = Convert.ToInt32(numero3);
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Entrada de " + texto3.Text + " no reconeguda.");
+                }
+            }
+
+
+            // llamamos al objeto geometria que contiene los métodos de cálculo
+            Geometria g = new Geometria();
+
+            switch (nom) // según el botón que hemos llamado al principio
+            {
+                case "boton_cuadrado":
+                    // asignamos el valor del método a la caja de texto resultado
+                    resultado.Text = Convert.ToString(g.areacuadrado(num1)); 
+                    break;
+                case "boton_circulo":
+                    resultado.Text = Convert.ToString(g.areaCirculo(num1));
+                    break;
+                case "boton_triangulo":
+                    resultado.Text = Convert.ToString(g.areatriangulo(num1, num2));
+                    break;
+                case "boton_rectangulo":
+                    resultado.Text = Convert.ToString(g.arearectangulo(num1, num2));
+                    break;
+                case "boton_pentagono":
+                    resultado.Text = Convert.ToString(g.areapentagono(num1, num2));
+                    break;
+                case "boton_rombo":
+                    resultado.Text = Convert.ToString(g.arearombo(num1, num2));
+                    break;
+                case "boton_romboide":
+                    resultado.Text = Convert.ToString(g.arearomboide(num1, num2));
+                    break;
+                case "boton_trapecio":
+                    resultado.Text = Convert.ToString(g.areatrapecio(num1, num2, num3));
+                    break;
+                default:
+                    break;
+            }
+            panel2.Show(); // mostramos el panel del resultado
+        }
+    }
+}
